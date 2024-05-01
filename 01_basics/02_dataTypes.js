acbdbf1b731c21e49094a6423b2177172dde89a0
aditish11 <96249916+aditish11@users.noreply.github.com>
2024-05-01 10:37:04 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as newer version
+
+//alert(2+3) we are using node.js and not browser
+
+let name="joy"
+let age=21
+let state =true
+
+/*
+//number
+//bigint
+//string
+//boolean
+//null => standalone value
+//undefined => value not assigned
+//symbol => for uniqueness
+
+//object
+
+
+
+ 
+*/
+
+console.log(typeof null);//it will print object
+console.log(typeof undefined);//it will print undefined
