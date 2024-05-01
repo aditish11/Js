3519d1e74fe08ddae117a53a5c6416ecebbe6c1c
aditish11 <96249916+aditish11@users.noreply.github.com>
2024-05-01 13:25:20 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+// == equality and comparisons >=,<=,<,> work differently , comparisons convert null to 0
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<0);
