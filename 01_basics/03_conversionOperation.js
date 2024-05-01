9254280d9643e050d3a04da19df684b2b59c3562
aditish11 <96249916+aditish11@users.noreply.github.com>
2024-05-01 10:59:40 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="2ww"
+console.log(typeof(score));
+let valueInNumber=Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "21" => 21
+// "21jdv" => Nan but type is number
+// true => 1
+//null => 0
+
+let val=1
+let valueInBoolean=Boolean(val)
+console.log(valueInBoolean);
+
+// 1 => true
+// "" => false
+// " " => true
+// "jsnbjr" => true
