99dc5ab139be046f9e1fc6ff136f8a5265b630eb
aditish11 <96249916+aditish11@users.noreply.github.com>
2024-05-01 13:14:08 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,8 @@
 let score="2ww"
-console.log(typeof(score));
+// console.log(typeof(score));
 let valueInNumber=Number(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
 
 // "21" => 21
 // "21jdv" => Nan but type is number
@@ -11,9 +11,47 @@ console.log(valueInNumber);
 
 let val=1
 let valueInBoolean=Boolean(val)
-console.log(valueInBoolean);
+// console.log(valueInBoolean);
 
 // 1 => true
 // "" => false
 // " " => true
 // "jsnbjr" => true
+
+
+let someNumber=3;
+let stringNumber=String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+// *****************Operations*******************
+
+let value=3;
+let negValue=-value;
+// console.log(negValue); -3
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+//console.log(2**2);
+
+let s1="hello"
+let s2=" name"
+let s3=s1+s2
+// console.log(s3);
+
+// console.log(1+"2")//12
+// console.log("1"+2)//12
+// console.log("1"+2+3)//123
+// console.log(2+3+"1");//51
+
+// console.log(true);//true
+// console.log(-true);//-1
+// console.log(true+true+true);//3
+// console.log(-false);//-0
+// console.log(-false+true);//1
+
+
+
