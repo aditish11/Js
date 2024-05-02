0703d4574436be7b7a1c828e3d1cc71c435b43e1
aditish11 <96249916+aditish11@users.noreply.github.com>
2024-05-02 11:41:02 +0000

    01_arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,2 +1,33 @@
 const myArr=[0,1,2,3,4,5];
-//js arrays are resizeable 
+const heros=["abcd","qwert","asdf"];
+const myArr2= new Array(1,2,3,4);
+
+//js arrays are resizeable and can contain mix data types
+//js array-copy operations create shallow copies(same reference point)
+
+//Array methods
+
+// myArr.push(6);
+// console.log(myArr);
+// myArr.pop();
+// console.log(myArr);
+
+// myArr.unshift(9);
+// console.log(myArr);
+// myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes(8));
+// console.log(myArr.indexOf(3))
+
+// console.log(myArr.join());
+
+//slice , splice
+// splice includes the last range and manipulates the original array
+const a1=myArr.slice(1,3);
+console.log(a1);
+console.log(myArr);
+const a2=myArr.splice(1,3);
+console.log(a2);
+console.log(myArr);
+
