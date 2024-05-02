8b6c287f84b951b39e2faeb28af2d59dc4907d2b
aditish11 <96249916+aditish11@users.noreply.github.com>
2024-05-02 12:01:23 +0000

    02_arrays

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,25 @@
+const marvel_heros=["thor","iron-man","spiderman"]
+const dc_heros=["superman","batman","flash"]
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][0]);
+
+// const new_heros=marvel_heros.concat(dc_heros);
+// console.log(new_heros);
+
+// const all_heros=[...marvel_heros,...dc_heros]
+// console.log(all_heros);
+
+// console.log(Array.isArray("abde"));
+// console.log(Array.isArray([1,2]));
+
+// console.log(Array.from("abcd"));
+// console.log(Array.from({name:"abcd"}.name));
+//we need to specify from keys or values from what we need to make an array
+
+// let s1=100
+// let s2=200
+// let s3=300
+
+// console.log(Array.of(s1,s2,s3));
