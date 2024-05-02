d9eeb5cbf76764e8d65ef7046975702ba745be51
aditish11 <96249916+aditish11@users.noreply.github.com>
2024-05-02 07:30:51 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name="joy"
+const repoCount=50
+// console.log(name+ " value "+ count);
+// console.log(`Hello my name is ${name}`)
+
+const gameName= new String("ludoKing");
+// console.log(gameName);
+
+//many functions like trim, slice, substring , includes , replace etc
