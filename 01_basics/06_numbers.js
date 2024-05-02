2b077a3af79539b84bed2a4b2ab7040eab1329d1
aditish11 <96249916+aditish11@users.noreply.github.com>
2024-05-02 07:59:24 +0000

    numbers and maths

---
+++ 01_basics/06_numbers.js
@@ -0,0 +1,19 @@
+const score=300;
+// console.log(score);
+
+const balance =new Number(100);
+// console.log(balance.toString());
+
+
+// console.log(balance.toFixed(2))
+// console.log(balance.toPrecision(2));
+
+const num=1000000
+// console.log(num.toLocaleString('en-IN'))
+
+///////++++++++++++++++MATHS+++++++++++++++/////////////
+
+// console.log(Math.abs(-3));
+// console.log(Math.round(4.5));
+//floor,ceil,min,max, random
+console.log(Math.ceil(Math.random()*6));
