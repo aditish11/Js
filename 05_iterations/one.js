6e4ab25dbaf646052153a6cd18f4978a07cbee3e
aditish11 <96249916+aditish11@users.noreply.github.com>
2024-05-06 11:06:38 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,29 @@
+
+for (let i = 0; i <= 10; i++) {
+    const element =  i;
+    if(i==5){
+        // console.log("five");
+    }
+    // console.log(i);
+}
+
+for (let i = 0; i <=10; i++) {
+    
+    // console.log(`Outer loop ${i}`)
+
+    for (let j = 0; j <=10; j++) {
+        // console.log(`Inner loop ${j} and Outer loop ${i}`)
+        
+    }
+    
+}
+
+const heroes=["flash", "spiderman", "batman"]
+for (let i = 0; i < heroes.length; i++) {
+    const element = heroes[i];
+    console.log(element);
+    
+}
+
+// break and continue
+
