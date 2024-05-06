fea86e1be6ee581608e943b23e80e6b5625a861a
aditish11 <96249916+aditish11@users.noreply.github.com>
2024-05-06 11:19:53 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,19 @@
+let  i=0
+while (i<=10) {
+    // console.log(`Value is ${i}`)
+    i=i+2
+}
+
+const myArr=["flash", "batman", "superman"]
+let j=0;
+while(j<myArr.length){
+    // console.log(myArr[j])
+    j++
+}
+
+let score=11;
+do {
+    console.log(`Score is ${score}`)
+    score++;
+} while (score<=10);
+//Score is 11 will be still printed as we are checking the condition afterwards
